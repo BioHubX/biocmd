64d7b8fbaa8304d1d000d211e91716083c0d4d3a
liuyaping <applesline@163.com>
2024-12-06 10:55:02 +0800

    add install_requires

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='biocmd',
-    version='0.1.0',
+    version='0.1.1',
     description='BioLab Client',
     author='Yaping-Liu',
     author_email='applesline@163.com',
     license='Apache License 2.0',
     url="https://github.com/BioHubX/biocmd",
     packages = ['biocmd'],
+    install_requires=[
+        'requests>=2.32.3',
+        'click',
+    ],
     entry_points={
         'console_scripts': [
             'biocmd=biocmd.entry:cli',
         ],
 
     },
-
-
 )
